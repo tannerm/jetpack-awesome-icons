db193f29bdc2e4fe3632e574fe8ded142c0b8936
Tanner Moushey <tanner@10up.com>
2014-06-27 22:42:36 -0700

    updates to plugin

--- jetpack-awesome-icons.php
+++ jetpack-awesome-icons.php
@@ -1,21 +1,21 @@
 <?php
-/*
-Plugin Name: Jetpack Awesome Icons
-Plugin URI: http://wordpress.org
-Description: Use FontAwesome icons instead of Jetpack icons for sharing buttons
-Version: 0.1.0
-Author: Tanner Moushey
-Author URI: http://tannermoushey.com
-License: GPL2
-*/
+/**
+ * Plugin Name: Jetpack Awesome Icons
+ * Plugin URI: https://github.com/tannerm/jetpack-awesome-icons
+ * Description: Use FontAwesome icons instead of Jetpack icons for sharing buttons
+ * Version: 0.1.0
+ * Author: Tanner Moushey
+ * Author URI: http://tannermoushey.com
+ * License: GPL2
+ */
 
-define( 'JETPACK_DEV_DEBUG', true);
-define( 'JAI_URL',     plugin_dir_url( __FILE__ ) );
+define( 'JAI_URL',     plugin_dir_url( __FILE__ )  );
 define( 'JAI_PATH',    plugin_dir_path( __FILE__ ) );
-define( 'JAI_VERSION', '0.1.0' );
-define( 'JAI_KEY', 'jai_enabled' );
+define( 'JAI_VERSION', '0.1.0'                     );
 
 function jai_init() {
+	add_filter( 'jetpack_development_mode', '__return_true' );
+
 	add_action( 'wp_enqueue_scripts',         'jai_enqueue_scripts'     );
 	add_action( 'load-settings_page_sharing', 'jai_enqueue_scripts'     );
 	add_action( 'sharing_global_options',     'jai_enable_custom_icons' );
@@ -24,7 +24,7 @@ function jai_init() {
 add_action( 'plugins_loaded', 'jai_init' );
 
 function jai_enqueue_scripts() {
-	if ( ! get_option( JAI_KEY ) ) {
+	if ( ! apply_filters( 'jai_enabled', get_option( 'jai_enabled' ) ) ) {
 		return;
 	}
 	wp_enqueue_style( 'jai-awesome', JAI_URL . '/assets/css/font-awesome.min.css', array(), JAI_VERSION );
@@ -33,11 +33,6 @@ function jai_enqueue_scripts() {
 
 function jai_enable_custom_icons() {
 	include_once( JAI_PATH . 'views/enable_custom_icons.php' );
-}
-
-function admin_init() {
-	if ( isset( $_GET['page'] ) && ( $_GET['page'] == 'sharing.php' || $_GET['page'] == 'sharing' ) )
-		$this->process_requests();
 }
 
 function jai_admin_init() {
@@ -49,6 +44,5 @@ function jai_admin_init() {
 		return;
 	}
 
-	update_option( JAI_KEY, isset( $_POST['jai-enable'] ) );
-
+	update_option( 'jai_enabled', isset( $_POST['jai-enable'] ) );
 }
